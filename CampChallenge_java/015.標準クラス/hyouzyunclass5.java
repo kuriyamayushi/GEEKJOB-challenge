4f02e88c477abdfaabddcc7560892a099e9ad28d
kuriyamayushi <yushi.business@gmail.com>
2018-02-21 11:10:43 +0900

    hyouzyunclass kadai5

---
+++ CampChallenge_java/015.標準クラス/hyouzyunclass5.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package hyouzyunnclass1;
+
+/**
+ *
+ * @author guest1Day
+ */
+public class hyouzyunclass5 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        String word = ("クリヤマユウシ");
+        
+        System.out.print(word.length());
+        
+        System.out.print("クリヤマユウシ".getBytes().length);
+        //System.out.print("クリヤマユウシ".getBytes("Shift-JIS").length);
+        
+    }
+    
+}
